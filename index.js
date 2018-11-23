78fcec0305da70fe72de0a2d2be8424a21fa152e
edpe <ed@edperkins.co.uk>
2018-11-23 15:57:54 +0000

    adjustments to mongo triggers

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 require('dotenv').config();
-const browserify = require('browserify-middleware');
+
 const Library = require('./library');
 const StoryModel = require('./storyModel');
 const express = require('express');
@@ -20,27 +20,9 @@ const sessions = {};
 
 const library = new Library(StoryModel);
 
-app.get('/js/charts.js', browserify(__dirname + '/charts.js'));
+
 
 app.use('/admin', express.static('client'));
-
-app.get('/stats/data.json', async (req, res, next) => {
-  const ooh_story1P = library.getStoryData('ooh_story1');
-  const ooh_story2P = library.getStoryData('ooh_story2');
-  const ooh_story3P = library.getStoryData('ooh_story3');
-  const ooh_story4P = library.getStoryData('ooh_story4');
-
-  let stories;
-  try {
-    stories = await Promise.all([ooh_story1P, ooh_story2P, ooh_story3P, ooh_story4P]);
-  } catch(e) {
-    next(e);
-
-    return e;
-  }
-
-  res.json(stories);
-});
 
 app.get('/', (req, res) => {
   // check if verification token is correct
@@ -62,55 +44,57 @@ app.post('/', (req, res) => {
     parameters: { save: 'me' },
   };
 
-  if (result.interaction.name.substring(0, 15) === 'choose ooh_story') {
+
+  if (result.interaction.name.substring(0, 16) === 'choose ooh_story') {
+      console.log(result.interaction.name.substring(7, 18))
     switch (result.interaction.name) {
       case 'choose ooh_story1':
-        library.logStoryChoice(result.interaction.name.substring(7, 16));
+        library.logStoryChoice(result.interaction.name.substring(7, 17));
         break;
       case 'choose ooh_story2':
-        library.logStoryChoice(result.interaction.name.substring(7, 16));
+        library.logStoryChoice(result.interaction.name.substring(7, 17));
         break;
       case 'choose ooh_story3':
-        library.logStoryChoice(result.interaction.name.substring(7, 16));
+        library.logStoryChoice(result.interaction.name.substring(7, 17));
         break;
       case 'choose ooh_story4':
-        library.logStoryChoice(result.interaction.name.substring(7, 16));
+        library.logStoryChoice(result.interaction.name.substring(7, 17));
         break;
     }
-  } else if (result.interaction.name.substring(0, 15) === 'end of ooh_story') {
+  } else if (result.interaction.name.substring(0, 16) === 'end of ooh_story') {
     switch (result.interaction.name) {
       case 'end of ooh_story1':
-        library.logStoryCompleted(result.interaction.name.substring(7, 16));
+        library.logStoryCompleted(result.interaction.name.substring(7, 17));
         break;
       case 'end of ooh_story2':
-        library.logStoryCompleted(result.interaction.name.substring(7, 16));
+        library.logStoryCompleted(result.interaction.name.substring(7, 17));
         break;
       case 'end of ooh_story3':
-        library.logStoryCompleted(result.interaction.name.substring(7, 16));
+        library.logStoryCompleted(result.interaction.name.substring(7, 17));
         break;
       case 'end of ooh_story4':
-        library.logStoryCompleted(result.interaction.name.substring(7, 16));
+        library.logStoryCompleted(result.interaction.name.substring(7, 17));
         break;
     }
-  } else if (result.interaction.name.substring(0, 13) === 'rate ooh_story') {
+  } else if (result.interaction.name.substring(0, 14) === 'rate ooh_story') {
     switch (result.interaction.name) {
       case 'rate ooh_story1':
         console.log('resolved query', result.resolvedQuery);
         library.addStoryRating(
-          result.interaction.name.substring(5, 14),
+          result.interaction.name.substring(5, 15),
           ratings[result.resolvedQuery]
         );
         break;
       case 'rate ooh_story2':
         console.log('resolved query', result.resolvedQuery);
         library.addStoryRating(
-          result.interaction.name.substring(5, 14),
+          result.interaction.name.substring(5, 15),
           ratings[result.resolvedQuery]
         );
         break;
       case 'rate ooh_story3':
         library.addStoryRating(
-          result.interaction.name.substring(5, 14),
+          result.interaction.name.substring(5, 15),
           ratings[result.resolvedQuery]
         );
         break;
